d5913f01c71f621502f48e359a5a784c259a397a
Brian <brianych@usc.edu>
2016-04-14 19:12:46 -0700

    INitial Camerea movement script. In progress

---
+++ JWF2/Assets/JWF/Scripts/JWFCameraMenuMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JWF
+{
+	public class JWFCameraMenuMover : MonoBehaviour
+	{
+		public void MoveCamera(JWFMenuState state)
+		{
+			switch( state )
+			{
+				case JWFMenuState.StartScreen:
+				{
+
+				}
+				break;
+
+				case JWFMenuState.GameLobby:
+				{
+
+				}
+				break;
+
+			}
+		}
+	}
+}
--- JWF2/Assets/JWF/Scripts/JWFMenuManager.cs
+++ JWF2/Assets/JWF/Scripts/JWFMenuManager.cs
@@ -4,17 +4,22 @@ using System.Collections;
 
 namespace JWF
 {
+	public enum JWFMenuState
+	{
+		StartScreen = 0,
+		GameLobby = 1
+	}
+
+	private List<Vector3> _playerStartPositions = new List<Vector3>()
+		{
+			new Vector3( 0, 0.71f,-10.36f),
+			new Vector3(10,0.71f,-10.36f),
+		};
+
 	public class JWFMenuManager : MonoBehaviour
 	{
 		JWFMenuActions keyboardListener;
 		JWFMenuActions joystickListener;
-
-		private enum MenuState
-		{
-			StartScreen,
-			Menu,
-			GameLobby
-		}
 
 		void OnEnable()
 		{
